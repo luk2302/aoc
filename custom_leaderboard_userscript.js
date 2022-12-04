1ce8de840ee0baaf963ee12f9e9dad7672307d25
Lukas Rinke <lukas.rinke@yahoo.de>
2022-12-04 18:08:06 +0100

    custom scoreboard supports individual days

--- custom_leaderboard_userscript.js
+++ custom_leaderboard_userscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         AOC custom score board
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/luk2302/aoc/blob/master/custom_leaderboard_userscript.js
 // @version      0.1
 // @description  AOC custom score board
 // @author       Lukas Rinke
-// @match        https://adventofcode.com/2022/leaderboard/private/view/*
+// @match        https://adventofcode.com/*/leaderboard/private/view/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=adventofcode.com
 // @grant        none
 // ==/UserScript==
@@ -12,24 +12,30 @@
 (function() {
     'use strict';
 
-    fetch(`${document.location.href.split("/").at(-1)}.json`).then(function(response) {
+    let parts = document.location.href.split("/")
+    fetch(`${parts[parts.length - 1]}.json`).then(function(response) {
         return response.json();
     }).then(function(data) {
         handle(data);
     });
 })();
 
-function handle(scoreboard_data) {
-    let members = Object.keys(scoreboard_data.members).map(function (key) {
-        return scoreboard_data.members[key];
+let players = []
+let year = parseInt(document.location.href.split("/")[3])
+
+function handle(scoreboardData) {
+    let members = Object.keys(scoreboardData.members).map(function (key) {
+        return scoreboardData.members[key];
     });
 
     members = members.filter(member => member.local_score !== 0)
     members.sort((a, b) => b.local_score - a.local_score)
 
     let now = new Date()
-    let day_to_check = now.getUTCHours() < 6 ? now.getUTCDate() - 1 : now.getUTCDate()
-    let start_of_aoc_day_ts = new Date(Date.UTC(2022, 11, day_to_check, 5)).getTime() / 1000
+    let dayToCheck = now.getUTCHours() < 6 ? now.getUTCDate() - 1 : now.getUTCDate()
+    if (year < now.getUTCFullYear()) {
+        dayToCheck = 25
+    }
 
     function showTime(day, time) {
         function pad(n) {
@@ -49,46 +55,150 @@ function handle(scoreboard_data) {
         }
     }
 
-    var maximum_score_length = 0
-    members = members.map(function (member) {
-        let first_star = member.completion_day_level[day_to_check.toString()]?.["1"]?.get_star_ts
-        let second_star = member.completion_day_level[day_to_check.toString()]?.["2"]?.get_star_ts
-        let daily_score_str = `* (${showTime(start_of_aoc_day_ts, first_star)} / ${showTime(start_of_aoc_day_ts, second_star)})`
-        maximum_score_length = Math.max(maximum_score_length, daily_score_str.length)
+    players = members.map(function (member) {
+        let days = []
+        for (let day = 0; day < dayToCheck; day++) {
+            let startOfAocDayTs = new Date(Date.UTC(year, 11, day + 1, 5)).getTime() / 1000
+            let firstStar = member.completion_day_level[String(day + 1)]?.["1"]?.get_star_ts
+            let secondStar = member.completion_day_level[String(day + 1)]?.["2"]?.get_star_ts
+            let dailyTimesStr = `* (${showTime(startOfAocDayTs, firstStar)} / ${showTime(startOfAocDayTs, secondStar)}) `
+            days.push({
+                "local_score": 0,
+                "local_score_original": member.local_score,
+                "daily_times_str": dailyTimesStr,
+                "first_star": firstStar,
+                "second_star": secondStar
+            })
+        }
         return {
             "name": member.name,
-            "local_score": member.local_score,
-            "daily_score_str": daily_score_str,
-            "first_star": first_star,
-            "second_star": second_star
+            "days": days,
         }
     })
 
-    let html_objects = [...document.getElementsByClassName("privboard-row")]
-    html_objects.shift()
-
-    for (let i = members.length; i < html_objects.length; i++) {
-        html_objects[i].remove()
+    let htmlObjects = getScoreboardRows()
+    for (let i = players.length; i < htmlObjects.length; i++) {
+        htmlObjects[i].remove()
     }
-    for (let i = 0; i < members.length; i++) {
-        html_objects[i].dataset.local_score = members[i].local_score
-        html_objects[i].dataset.first_star = members[i].first_star
-        html_objects[i].dataset.second_star = members[i].second_star
-        html_objects[i].dataset.second_star_offset = (members[i].second_star && members[i].first_star) ? members[i].second_star - members[i].first_star : undefined
-        html_objects[i].children[day_to_check].textContent = members[i].daily_score_str.padEnd(maximum_score_length, " ")
+    for (let i = 0; i < players.length; i++) {
+        htmlObjects[i].dataset.playerIndex = String(i)
     }
-
+    computeScores(htmlObjects.length)
+    setScoreboardAttributes(dayToCheck)
     setupSorting()
+    setupDaySelection(dayToCheck)
+    createCss()
+}
+
+function computeScores(numUnfilteredPlayers) {
+    function compute(attribute, day) {
+        const sortedFirst = [...players].sort((a, b) => {
+            if (a.days[day][attribute] === undefined) {
+                return 1
+            }
+            if (b.days[day][attribute] === undefined) {
+                return -1
+            }
+            return a.days[day][attribute] - b.days[day][attribute]
+        });
+        for (let playersBefore = 0; playersBefore < sortedFirst.length; playersBefore++) {
+            let gained = numUnfilteredPlayers - playersBefore
+            if (sortedFirst[playersBefore].days[day][attribute] !== undefined) {
+                sortedFirst[playersBefore].days[day].local_score += gained
+            }
+        }
+    }
+    for (let day = 0; day < players[0].days.length; day++) {
+        if (day > 0) {
+            for (let i = 0; i < players.length; i++) {
+                players[i].days[day].local_score = players[i].days[day - 1].local_score
+            }
+        }
+        compute("first_star", day)
+        compute("second_star", day)
+    }
+}
+
+function toggleClass(cls, ele) {
+    [].forEach.call(document.getElementsByClassName(cls), function(el) {
+        el.classList.remove(cls);
+    });
+    ele.classList.add(cls)
+}
+
+function setupDaySelection(days) {
+    let dayElements = document.getElementsByClassName("privboard-days")[0].children
+    for (let day = 0; day < days; day++) {
+        dayElements[day].dataset.day = String(day)
+        dayElements[day].addEventListener("click", selectDay)
+    }
+    dayElements[days - 1].classList.add("activeDay")
+}
+
+function selectDay(e) {
+    if (e.target.classList.contains("activeDay")) {
+        return
+    }
+
+    toggleClass("activeDay", e.target)
+    let day = parseInt(e.target.dataset.day) + 1
+    setScoreboardAttributes(day)
+    document.getElementsByClassName("activeSort")[0].click()
+
+    e.preventDefault()
+    return false
+}
+
+function createCss() {
+    let css = '.activeSort { color: lightgreen; } .activeDay { color: lightgreen; }'
+    let style = document.createElement('style')
+    document.head.appendChild(style);
+    style.appendChild(document.createTextNode(css));
+}
+
+function getScoreboardRows() {
+    let htmlObjects = [...document.getElementsByClassName("privboard-row")]
+    htmlObjects.shift()
+    return htmlObjects
+}
+
+let lastSelectedDay = 0
+function setScoreboardAttributes(day) {
+    day--
+    let htmlObjects = getScoreboardRows()
+    let maximumTimesLength = 0;
+    let maximumScoreLength = 0;
+    for (let i = 0; i < players.length; i++) {
+        maximumTimesLength = Math.max(maximumTimesLength,  players[i].days[day].daily_times_str.length)
+        maximumScoreLength = Math.max(maximumScoreLength,  String(players[i].days[day].local_score).length)
+    }
+    for (let i = 0; i < players.length; i++) {
+        let player = players[parseInt(htmlObjects[i].dataset.playerIndex)]
+        htmlObjects[i].dataset.local_score_str = String(player.days[day].local_score).padStart(maximumScoreLength, " ")
+        htmlObjects[i].dataset.local_score = player.days[day].local_score
+        htmlObjects[i].dataset.first_star = player.days[day].first_star
+        htmlObjects[i].dataset.second_star = player.days[day].second_star
+        htmlObjects[i].dataset.second_star_offset = (player.days[day].second_star && player.days[day].first_star) ? player.days[day].second_star - player.days[day].first_star : undefined
+        htmlObjects[i].children[lastSelectedDay + 1].textContent = "*"
+        htmlObjects[i].children[day + 1].textContent = player.days[day].daily_times_str.padEnd(maximumTimesLength, " ")
+    }
+    lastSelectedDay = day
 }
 
 function sort(e) {
+    toggleClass("activeSort", e.target)
+
     let attribute = e.target.dataset.attribute
     let inverted = e.target.dataset.inverted
-    let html_objects = [...document.getElementsByClassName("privboard-row")]
-    let scoreboard = html_objects[0].parentElement
-    html_objects.shift()
+    sortInternal(attribute, inverted)
+}
 
-    html_objects.sort((a, b) => {
+function sortInternal(attribute, inverted) {
+    let htmlObjects = [...document.getElementsByClassName("privboard-row")]
+    let scoreboard = htmlObjects[0].parentElement
+    htmlObjects.shift()
+
+    htmlObjects.sort((a, b) => {
         if (a.dataset[attribute] === 'undefined') {
             return inverted
         }
@@ -98,13 +208,15 @@ function sort(e) {
         return (a.dataset[attribute] - b.dataset[attribute]) * inverted
     })
 
-    for (let i = 0; i < html_objects.length; i++) {
-        scoreboard.appendChild(html_objects[i])
-        html_objects[i].children[0].textContent = ` ${String(i+1).padStart(2, " ")})`
+    let num = Math.ceil(Math.log10(players.length + 1))
+    for (let i = 0; i < htmlObjects.length; i++) {
+        scoreboard.appendChild(htmlObjects[i])
+        htmlObjects[i].children[0].textContent = ` ${String(i+1).padStart(num, " ")})`
+        htmlObjects[i].childNodes[1].textContent = ` ${htmlObjects[i].dataset.local_score_str} `
     }
 }
 
-function addButton(text, attribute, inverted) {
+function createButton(text, attribute, inverted) {
     let element = document.createElement("a")
     element.href = "javascript:void(0)"
     element.dataset.attribute = attribute
@@ -119,12 +231,14 @@ function setupSorting() {
     let p = document.createElement("p")
     scoreboard.parentElement.insertBefore(p, scoreboard)
     p.appendChild(document.createTextNode("Sort by "))
-    p.appendChild(addButton("Score", "local_score", -1))
+    let firstButton = createButton("Score", "local_score", -1)
+    p.appendChild(firstButton)
+    firstButton.click()
     p.appendChild(document.createTextNode(", "))
-    p.appendChild(addButton("First star", "first_star", 1))
+    p.appendChild(createButton("First star", "first_star", 1))
     p.appendChild(document.createTextNode(", "))
-    p.appendChild(addButton("Second star", "second_star", 1))
+    p.appendChild(createButton("Second star", "second_star", 1))
     p.appendChild(document.createTextNode(" or "))
-    p.appendChild(addButton("Second star offset", "second_star_offset", 1))
+    p.appendChild(createButton("Second star offset", "second_star_offset", 1))
     p.appendChild(document.createTextNode("."))
 }
